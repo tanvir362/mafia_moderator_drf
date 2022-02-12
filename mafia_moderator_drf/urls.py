0bb3c449885c97afd3264e81829e22efcf4a5c97
Tanvir Ahmed <tanvir66288@gmai.com>
2022-02-12 23:52:05 +0600

    TA | rearranged urls

--- mafia_moderator_drf/urls.py
+++ mafia_moderator_drf/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('api.urls')),
 ]
