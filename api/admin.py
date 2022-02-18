f7b19c1bd72514b4a67dfcdb6265709e0cdb02d5
Tanvir Ahmed <tanvir66288@gmai.com>
2022-02-19 00:43:37 +0600

    TA | ragistered RoundAdmin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
+import imp
 from django.contrib import admin
+from api.models import Round
 
 # Register your models here.
+@admin.register(Round)
+class RoundAdmin(admin.ModelAdmin):
+    list_display = ['id', 'team_id', 'channel_id', 'is_night']
