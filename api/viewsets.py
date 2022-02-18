a1fb9a413a33f780206db1031154a5daf251f63c
Tanvir Ahmed <tanvir66288@gmai.com>
2022-02-19 00:49:24 +0600

    TA | updated new_round viewset method to start a round

--- api/viewsets.py
+++ api/viewsets.py
@@ -2,11 +2,10 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from slack_sdk import WebClient
 import os
 from django.conf import settings
-
-
+from api.models import Round
+from api.services import send_message
 
 
 class SlackAppViewset(viewsets.ReadOnlyModelViewSet):
@@ -15,19 +14,31 @@ class SlackAppViewset(viewsets.ReadOnlyModelViewSet):
     def get_serializer_class(self, *args, **kwargs):
         pass
     
-    @action(methods=['POST'],detail = False)
+    @action(methods=['POST'], detail=False)
     def new_round(self, request, *args, **kwargs):
-        data = request.data
-        # print(data)
-
-        client = WebClient(token=os.getenv('BOT_AUTH_TOKEN'))
-        client.chat_postMessage(
-            channel=os.getenv('CHANNEL'),
-            text="Hello from your app! :tada:"
-        )
+        payload = request.data
+        print(payload)
+        team = payload["team_id"]
         
-        return Response(data, status=status.HTTP_200_OK)
+        if Round.objects.filter(team_id=team).exists():
+            send_message(
+                channel=os.getenv('CHANNEL'),
+                text="A round is going on"
+            )
 
+        else:
+
+            round = Round.objects.create(
+                team_id = team,
+                channel_id = payload["channel_id"]
+            )
+
+            send_message(
+                channel=os.getenv('CHANNEL'),
+                text="Now round started.\nEveryone, please run /my_role command to get your role"
+            )
+        
+        return Response(status=status.HTTP_200_OK)
 
     @action(methods=['GET'],detail = False)
     def test(self, request, *args, **kwargs):
